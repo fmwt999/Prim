daa41aa37b98ef8ec1d9bc147ae801d86283033b
fmwt999 <138676550+fmwt999@users.noreply.github.com>
2023-07-05 18:54:32 +0700

    Initial commit

---
+++ Prim/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prim
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Butt_Click(object sender, RoutedEventArgs e)
+        {
+            int num1 = int.Parse(Num1_TB.Text);
+            int num2 = int.Parse(Num2_TB.Text);
+            int res = num1 * 2 + num2 * 2;
+            Res_TB.Text = res.ToString();
+        }
+    }
+}
